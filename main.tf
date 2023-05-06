e6e72b39a2b0ed78728ac6fcc9fdf0997ef04398
rhananga <107822688+rhananga@users.noreply.github.com>
2023-05-06 15:09:08 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+# provider.tf
+provider "aws" {
+  region = "us-east-1"
+  #access_key = var.access_key
+  #secret_key = var.secret_key
+}
+
+# main.tf
+resource "aws_vpc" "testvpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "testsubnet" {
+  vpc_id     = aws_vpc.testvpc.id
+  cidr_block = "10.0.1.0/24"
+}
